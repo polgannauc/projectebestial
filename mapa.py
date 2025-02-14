9f0e64166d393f9ba31a7875e053f1fb146622c0
oscarmm-lacetania <oscar.martinez24@lacetania.cat>
2025-02-14 19:54:58 +0100

    Actaulizació del mapa dels collons
    De moment puc imprimir un mapa amb elements aleatoris d'una llista i amb l'explorador al centre.
    Però s'ha de tenir en compte més coses

--- mapa.py
+++ mapa.py
@@ -1,1 +1,24 @@
-print("hola mundo")
+# Generació del tauler (mapa) i la col·locació dels elements
+# També gestionar les regles de cada element
+
+import random
+import elements
+
+def imprimir_mapa(fila, columna, llista_elements):
+    columna+=1
+    for i in range(fila):
+        print("+---" * (columna-1) + "+")
+        for j in range(columna):
+            caracter = llista_elements[random.randint(0,len(llista_elements)-1)]
+            if j == columna-1:
+                print("|")
+            elif j==2 and i ==2:
+                print(f"| E ", end ="")
+            else:
+                print(f"| {caracter}", end ="")
+    print("+---" * (columna-1) + "+")
+
+
+elements = ["X ", ". ","A "]
+        
+imprimir_mapa(5,5,elements)
