ddd3bc9ad99c4c1d56f4612c3932751a818c7f6d
polgannauc <pol.gannau24@lacetania.cat>
2025-02-28 19:26:53 +0100

    Posicio jugador
    guardar una posicio aleatoria del jugador treient-lo de la llista de elements. Inici de prints de elements en el mapa.

--- elements.py
+++ elements.py
@@ -1,30 +1,26 @@
 import var_globals
 import random
-# On es troben definits tots el elements
 
-## Llista d'elements:
-### Casella oculat (X)
-### Casella destapada (.)
-### Animals (A)
-### Trampes (T)
-### Llac (L)
-### Bosc (B)
-### Refugi (R)
-### Caçador (C)
-
-#L’objectiu del joc és trobar-los i fotografiar-los.
-# Suma punts d’energia per cada animal fotografiat (segons el nivell) i es guarda a la llista d’animals fotografiats.
-# Un cop la casella ha estat visitada s’ha de mostrar el símbol de la casella destapada.
+dic_pos = {}
 
 # Funció per generar un diccionari amb cada element com a clau i els valors com llistes de tuples amb les posicions
 # Accepta com a paràmetres: mida del mapa, llista amb els elements, diccionari amb quantitat de cada element segons el nivell
-def generar_posicions(mida,ll,diccionari):
-    dic_pos = {}
+
+def generar_posicions(mida, ll, diccionari):
+    global dic_pos
     combinacions_possibles = [(x, y) for x in range(mida) for y in range(mida)]
     random.shuffle(combinacions_possibles)
+    pos_jugador = combinacions_possibles.pop()
+    var_globals.jugador_x = pos_jugador[0]
+    var_globals.jugador_y = pos_jugador[1]
     for i in ll:
         ll_aux = []
         for j in range(diccionari[i]):
             ll_aux.append(combinacions_possibles.pop())
         dic_pos[i]=ll_aux
     return dic_pos
+
+def elements():
+    for clau, valors in dic_pos:
+        for valor in valors:
+            print(clau)
