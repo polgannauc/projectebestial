9b58e5ea67784791408e19d73a7a56c4855a237e
oscarmm-lacetania <oscar.martinez24@lacetania.cat>
2025-02-28 19:27:30 +0100

    Canvis a mapa i var_globals
    En el mapa he posat la funció de generar visió i he tret variables que ja les tenim a global.
    A global he modificat la llista d'elements i he tret la funció de visió. El jugadot té com a valors inicials 0,0

--- var_globals.py
+++ var_globals.py
@@ -5,36 +5,13 @@ import random
 
 gameplay = False
 level = 0
-jugador_x = 2
-jugador_y = 2
+jugador_x = 0
+jugador_y = 0
 #animal_x = 1
 #animal_y = 1
 jugador_vida = 0
 entitats = {}
 mida = 0
 
-
-# Funció per generar la visibilitat segons el nivell de dificultat
-# Funció per generar la visibilitat segons el nivell de dificultat
-# Genera una llista amb les coordenades que pot veure l'explorador
-# Paràmetres: mida que fa el mapa i les coordenades del jugador
-def generar_visio(mida, pos_jugador):
-    ll_tuples = []
-    direccions = []
-    match mida:
-        case 5:
-            direccions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, 1), (-1, -1), (1, 1), (1, -1), (-2, 0), (2, 0), (0, 2), (0, -2)]
-        case 10:
-            direccions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, 1), (-1, -1), (1, 1), (1, -1)]
-        case 15:
-            direccions = [(-1, 0), (1, 0), (0, -1), (0, 1)]    
-    x, y = pos_jugador[0], pos_jugador[1]
-    for dx, dy in direccions:
-        nou_dx = (x + dx) % mida
-        nou_dy = (y + dy) % mida
-        ll_tuples.append((nou_dx, nou_dy))
-    return(ll_tuples)
-
-
 #Llista amb tots els elements
-ll_elements = ["E","A","T","R","L","B","C"] 
+ll_elements = ["A","T","R","L","B","C"]
