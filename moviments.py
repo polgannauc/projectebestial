2dbcf675806c70f691d585e57d8a1808f93553cb
oscarmm-lacetania <oscar.martinez24@lacetania.cat>
2025-03-02 18:09:32 +0100

    Update moviments.py

--- moviments.py
+++ moviments.py
@@ -1,28 +1,27 @@
 # S'implementen les regles del moviment de l'explorador
 
 import keyboard
-import time
-import ppal
+import var_globals
 
 
 def desplaçament(mida,x,y):
     if keyboard.is_pressed("w"):
         x -= 1
-        if x== -1:
+        if x == -1:
             x=mida-1
     elif keyboard.is_pressed("s"):
         x += 1
-        if x== mida:
+        if x == mida:
             x=0
     elif keyboard.is_pressed("a"):
         y -= 1
-        if y==-1:
-            y=mida-1
+        if y == -1:
+            y=mida - 1
     elif keyboard.is_pressed("d"):
         y += 1
-        if y==mida:
-            y=0
+        if y == mida:
+            y = 0
     elif keyboard.is_pressed("q"):
-        ppal.gameplay = False
-        print("Sortint del joc...")
+        var_globals.gameplay = False
+        print("Sortint del joc...\nGràcies per jugar!!\n")
     return x,y
