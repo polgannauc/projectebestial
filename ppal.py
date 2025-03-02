da5edd8eb5fdca6c2a08ccd79346aeb43849404b
oscarmm-lacetania <oscar.martinez24@lacetania.cat>
2025-03-02 18:08:55 +0100

    Update ppal.py
    He fet un merge des de la torre sense voler i s'ha canviat tot a quan ho vaig modificar divendres pel matí.
    Ho estic posant tot al seu lloc

--- ppal.py
+++ ppal.py
@@ -1,16 +1,13 @@
-import random
 import var_globals
 import mapa
 
-gameplay = False
-level = 0
 
 def set_level(nivell):
-    global level
-    level = nivell
+    var_globals.level
+    var_globals.level = nivell
 
 def get_level():
-    return level
+    return var_globals.level
 
 def main():
     print("Benvingut/uda al nostre joc")
@@ -18,11 +15,14 @@ def main():
     print("\n\tNivell 1: Piece of cake!")
     print("\n\tNivell 2: Let's rock!")
     print("\n\tNivell 3: Come get some!")
-    nivell = get_level()
-    while nivell not in [1,2,3]:
-        nivell = int(input("Escull nivell: "))
-    set_level(nivell)
-    match nivell:
+
+    dificultat = get_level()
+
+    while dificultat not in [1,2,3]:
+        dificultat = int(input("\nEscull nivell: "))
+
+    set_level(dificultat)
+    match dificultat:
         case 1:
             print("\nHas escollit el nivell 1: Piece of cake!")
         case 2:
