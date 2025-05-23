b5d535a0e66490b092fb896706f4afa6190031ed
oscarmm-lacetania <oscar.martinez24@lacetania.cat>
2025-05-23 17:00:07 +0200

    canvis ppal

--- ppal.py
+++ ppal.py
@@ -22,15 +22,11 @@ def main():
                 if dificultat == 4:
                     try:
                         dades = var_globals.carregar_partida()
-                        var_globals.level = int(dades[0])
-                        var_globals.mida = int(dades[1])
+                        var_globals.level, var_globals.mida = int(dades[0]), int(dades[1])
                         elements_destapats = eval(dades[2])
-                        var_globals.max_animals = int(dades[3])
-                        var_globals.jugador_vida = int(dades[4])
-                        var_globals.jugador_x = int(dades[5])
-                        var_globals.jugador_y = int(dades[6])
-                        var_globals.caselles_destapades = eval(dades[7])
-                        var_globals.comptador_animals = int(dades[8])
+                        var_globals.max_animals, var_globals.jugador_vida = int(dades[3]), int(dades[4])
+                        var_globals.jugador_x, var_globals.jugador_y = int(dades[5]), int(dades[6])
+                        var_globals.caselles_destapades, var_globals.comptador_animals = eval(dades[7]), int(dades[8])
                         var_globals.inventari = eval(dades[9])
                         print("\nPartida carregada correctament")
                         seguir = True
@@ -66,11 +62,11 @@ def main():
         mapa.mapa_tapat(var_globals.mida, elements_destapats, camp_visio, var_globals.jugador_x, var_globals.jugador_y)
 
         if var_globals.jugador_vida<=0: # Condició derrota
-            var_globals.gameplay= False
+            var_globals.gameplay = False, 
             print("Has perdut")
         elif var_globals.comptador_animals == var_globals.max_animals: # Condició victòria
-            var_globals.gameplay = False 
-            print("Has guanyat!")
+            var_globals.gameplay = False
+            print("Has guanyat?")
         else:
             elements_destapats = moviments.desplaçament_jugador(elements_destapats)
             elements_destapats = elements.modificar_vida(elements_destapats, var_globals.inventari)
@@ -85,22 +81,20 @@ def main():
                         eleccio = int(entrada)
                         if eleccio in [1, 2, 3]:
                             seguir = True
+                            elements.utilitzar_objecte(eleccio, var_globals.inventari)
+                            var_globals.activacio_inventari = False
                         else:
-                            print("\n" * 15 + "Error: Només es pot escollir l'ampolla o el ganivet")
+                            print("\n" * 15 + "Error: Opció no vàlida")
                     except ValueError:
-                        if entrada:
-                            print("\n" * 15 + "Error: Has d'introduir un número per seleccionar l'objecte.")
-                        else:
-                            print("\n" * 15 + "Error: No has introduït res.")
-                elements.utilitzar_objecte(eleccio, var_globals.inventari)
-                var_globals.activacio_inventari = False
+                        missatge_error = "Error: Has d'introduir un número" if entrada else "Error: No has introduït res"
+                        print("\n" * 15 + missatge_error)
 
         if var_globals.visio_completa > 0: # En cas d'activar-se la fada, en cada torn es va descontant el comptador de visió completa
             var_globals.visio_completa -= 1
 
         if var_globals.guardar == True:
+            dades = [var_globals.level , var_globals.mida, elements_destapats, var_globals.max_animals, var_globals.jugador_vida, var_globals.jugador_x, var_globals.jugador_y, var_globals.caselles_destapades, var_globals.comptador_animals, var_globals.inventari]
             try:
-                dades = [var_globals.level , var_globals.mida, elements_destapats, var_globals.max_animals, var_globals.jugador_vida, var_globals.jugador_x, var_globals.jugador_y, var_globals.caselles_destapades, var_globals.comptador_animals, var_globals.inventari]
                 var_globals.guardar_partida(dades)
                 print("\nLa partida s'ha desat correctament")
                 var_globals.gameplay = False
